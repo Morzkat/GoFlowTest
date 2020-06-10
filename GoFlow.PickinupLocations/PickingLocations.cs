97dff110d5f31097ea59ac729c1c4134f702eb53
Felix David Garcia <felix.david21@outlook.com>
2020-06-10 12:05:14 -0400

    Fix bugs and improve logic for pickup locations

--- GoFlow.PickinupLocations/PickingLocations.cs
+++ GoFlow.PickinupLocations/PickingLocations.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -14,6 +13,12 @@ namespace GoFlow.InventoryPickingLocations
     {
         public int LocationId { get; set; }
         public int Quantity { get; set; }
+
+        public InventoryToPick(int locationId, int quantity)
+        {
+            LocationId = locationId;
+            Quantity = quantity;
+        }
     }
 
     class PickingLocations
@@ -27,78 +32,93 @@ namespace GoFlow.InventoryPickingLocations
             this.defaultUnitOfMeasure = defaultUnitOfMeasure;
         }
 
-        public List<InventoryToPick> Calculate(int quantityToPick)
+        public IEnumerable<InventoryToPick> Calculate(int quantityToPick)
         {
             // Implement the logic here
             var startIndex = 0;
-            var totalLocations = locations.Count;
-            var pickupLocations = new List<Location>() { new Location { Id = 0, QuantityAvailable = 0 } };
+            var locationsCount = locations.Count;
+            var pickupLocations = new Dictionary<int, InventoryToPick>();
+            var locationSelected = new Location { Id = 0, QuantityAvailable = 0 };
+            var boxQuantityToPick = quantityToPick / defaultUnitOfMeasure;
 
             if (!locations.Any())
                 return new List<InventoryToPick>();
 
-            for (int i = 0; i < totalLocations; i++)
+            for (int i = 0; i < locationsCount; i++)
             {
-                if ((locations[i].QuantityAvailable >= quantityToPick))
+                if (locations[i].QuantityAvailable >= quantityToPick)
                 {
                     if (locations[i].QuantityAvailable == quantityToPick)
                     {
-                        pickupLocations[startIndex] = locations[i];
-                        return MapLocationToInventoryToPick(pickupLocations);
+                        pickupLocations[startIndex] = new InventoryToPick(locations[i].Id, quantityToPick);
+                        return pickupLocations.Values;
                     }
 
-                    else if ((locations[i].QuantityAvailable < pickupLocations[startIndex].QuantityAvailable))
+                    else if (locations[i].QuantityAvailable < locationSelected.QuantityAvailable)
                     {
-                        pickupLocations[startIndex] = locations[i];
+                        pickupLocations[startIndex] = new InventoryToPick(locations[i].Id, quantityToPick);
+                        locationSelected = locations[i];
                         continue;
                     }
 
-                    else if (pickupLocations[startIndex].QuantityAvailable < quantityToPick)
+                    else if (locationSelected.QuantityAvailable < quantityToPick)
                     {
-                        pickupLocations[startIndex] = locations[i];
-                        continue;
+                        pickupLocations[startIndex] = new InventoryToPick(locations[i].Id, quantityToPick);
+                        locationSelected = locations[i];
                     }
-                }
-                else if ((locations[i].QuantityAvailable < quantityToPick) && pickupLocations[startIndex].QuantityAvailable < quantityToPick)
-                {
-                    var bestPickUpLocations = FindBestPickUpLocations(locations, i, totalLocations, quantityToPick);
-
-                    if (bestPickUpLocations.Any())
-                        return MapLocationToInventoryToPick(bestPickUpLocations);
                 }
             }
-            return MapLocationToInventoryToPick(pickupLocations);
+
+            if (pickupLocations.Count == 0)
+                return FindBestPickUpLocations(locations, quantityToPick, boxQuantityToPick);
+
+            return pickupLocations.Values;
         }
 
-        public List<Location> FindBestPickUpLocations(List<Location> locations, int startIndex, int endIndex, int quantityToPick)
+        public List<InventoryToPick> FindBestPickUpLocations(List<Location> locations, int quantityToPick, double boxQuantityToPick)
         {
-            var currentQuantity = 0;
-            var bestPickUpLocations = new List<Location>();
-            for (int i = startIndex; i < endIndex; i++)
-            {
-                var locationsToTake = endIndex - startIndex;
-                currentQuantity += locations[i].QuantityAvailable;
+            var bestPickUpLocations = new List<InventoryToPick>();
 
-                if (currentQuantity == quantityToPick)
-                    bestPickUpLocations = locations.GetRange(startIndex, locationsToTake);
+            foreach (var location in locations)
+            {
+                int boxQuantityInLocation = location.QuantityAvailable / defaultUnitOfMeasure;
+
+                if (boxQuantityInLocation >= 1)
+                {
+                    int i;
+                    for (i = 0; i < boxQuantityInLocation;)
+                    {
+                        i++;
+                        boxQuantityToPick--;
+                        quantityToPick -= defaultUnitOfMeasure;
+
+                        if (boxQuantityToPick == 0)
+                            break;
+                    }
+                    var quantityToPickFromLocation = i * defaultUnitOfMeasure;
+                    bestPickUpLocations.Add(new InventoryToPick(location.Id, quantityToPickFromLocation));
+                    location.QuantityAvailable -= quantityToPickFromLocation;
+                }
+
+                if (boxQuantityToPick == 0)
+                    break;
             }
+
+            locations.RemoveAll(x => x.QuantityAvailable == 0);
+
+            while (quantityToPick > 0)
+            {
+                foreach (var location in locations)
+                {
+                    bestPickUpLocations.Add(new InventoryToPick(location.Id, quantityToPick));
+                    quantityToPick -= location.QuantityAvailable;
+
+                    if (quantityToPick < 1)
+                        break;
+                }
+            }
+
             return bestPickUpLocations;
-        }
-
-        public List<InventoryToPick> MapLocationToInventoryToPick(List<Location> pickupLocations)
-        {
-            var inventoryToPick = new List<InventoryToPick>();
-
-            foreach (var pickupLocation in pickupLocations)
-            {
-                inventoryToPick.Add(new InventoryToPick
-                {
-                    LocationId = pickupLocation.Id,
-                    Quantity = pickupLocation.QuantityAvailable
-                });
-            }
-
-            return inventoryToPick;
         }
     }
 }
